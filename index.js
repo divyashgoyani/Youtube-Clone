1c461d086b39e8074ed79b1644e45e115b0fc04c
Divyash Goyani <divyashgoyani19@gmail.com>
2024-02-07 00:54:46 +0530

    Update File

--- index.js
+++ index.js
@@ -5,40 +5,44 @@ let div = document.getElementById("videodiv");
 async function display(){
     let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=webdevelopment%20video&key=AIzaSyBsc16hVF__FKR0CrAC_uogrowrV37p_n8&maxResults=20`);
     let data = await res.json();
-    console.log('>>>', data);
+    // console.log('>>>', data);
     
     for({id:{videoId}}of data.items) {
-        let videodiv= document.createElement("iframe");
-        videodiv.setAttribute("class","mons");
-        videodiv.src = `https://www.youtube.com/embed/${videoId}`;
-        videodiv.allow = 'fullscreen';
-        div.append(videodiv);
+        if(videoId != undefined) {
+            let videodiv= document.createElement("iframe");
+            videodiv.setAttribute("class","mons");
+            videodiv.src = `https://www.youtube.com/embed/${videoId}`;
+            videodiv.allow = 'fullscreen';
+            div.append(videodiv);
+        }
     }
 }
 display();
 
 
 //! Search When Something is typed in the search-box
-
 async function searchVideos(){
     document.getElementById("videodiv").innerHTML="";
     let query = document.getElementById("video").value;
 
     let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&key=AIzaSyBsc16hVF__FKR0CrAC_uogrowrV37p_n8&maxResults=20`);
     let data = await res.json();
-    console.log('>>>>>', data);
+    // console.log('>>>>>', data);
 
     for({id:{videoId}}of data.items) {
-        let videodiv= document.createElement("iframe");
-        videodiv.setAttribute("class","mons");
-        videodiv.src = `https://www.youtube.com/embed/${videoId}`;
-        videodiv.allow = 'fullscreen';
-        div.append(videodiv);
+        if(videoId != undefined) {
+            let videodiv= document.createElement("iframe");
+            videodiv.setAttribute("class","mons");
+            videodiv.src = `https://www.youtube.com/embed/${videoId}`;
+            videodiv.allow = 'fullscreen';
+            div.append(videodiv);
+        }
     }
 }
 
-function Home(){
-    document.getElementById("video").textContent = "";
+function home(){
+    document.getElementById("video").value = "";
+    display();
 }
 
 // Devang Key = AIzaSyAxG--_HGPJlv0O3gNPXjywxf7fxCcsLPg
